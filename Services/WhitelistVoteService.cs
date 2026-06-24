4ef3647e036172a6166404676d23f5691823e3f5
sleepyyapril <flyingkarii@gmail.com>
2026-06-24 20:08:44 -0300

    fix auth url 2

--- Services/WhitelistVoteService.cs
+++ Services/WhitelistVoteService.cs
@@ -13,7 +13,7 @@ public class WhitelistVoteService(IServiceProvider services)
     private const ulong WhitelistChannel = 1302308802619773062;
     private const ulong WhitelistVoteChannel = 1315318721836879942;
 
-    private const string NameApi = "https://auth.spacestation14.com/api/query/name?name=";
+    private const string NameApi = "https://auth.playss14.com/api/query/name?name=";
     
     public Task InitializeAsync()
     {
